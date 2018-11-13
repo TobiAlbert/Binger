b5691e03eb8877d5b611592319d670dae7f8f660
Tobi Daada <daadatobi@gmail.com>
2018-11-13 12:48:38 +0100

    Implement Adapter views from custom layout

--- app/src/main/java/ng/max/binger/adapters/TvShowsAdapter.kt
+++ app/src/main/java/ng/max/binger/adapters/TvShowsAdapter.kt
@@ -1,30 +1,69 @@
 package ng.max.binger.adapters
 
+import android.content.Context
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.Toast
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.list_item_tv_show.view.*
 import ng.max.binger.R
 import ng.max.binger.data.TvShow
+import ng.max.binger.utils.DisplayUtils
 
-class TvShowsAdapter: RecyclerView.Adapter<TvShowsAdapter.ViewHolder>() {
+class TvShowsAdapter: RecyclerView.Adapter<TvShowsAdapter.TvShowViewHolder>() {
 
-    private var tvShows: ArrayList<TvShow> = arrayListOf()
+    var tvShows = emptyList<TvShow>()
+        set(value) {
+            field = value
+            notifyDataSetChanged()
+        }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.list_item_tv_show, parent, false)
-        return ViewHolder(view)
+    private lateinit var mContext: Context
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TvShowViewHolder {
+        mContext = parent.context
+        val view = LayoutInflater.from(mContext).inflate(R.layout.list_item_tv_show, parent, false)
+        return TvShowViewHolder(view)
     }
 
     override fun getItemCount() = tvShows.size
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) = holder.bind(tvShows[position])
+    override fun onBindViewHolder(holder: TvShowViewHolder, position: Int) = holder.bind(tvShows[position])
 
-
-    inner class ViewHolder(view: View): RecyclerView.ViewHolder(view) {
+    inner class TvShowViewHolder(view: View): RecyclerView.ViewHolder(view) {
 
         fun bind(tvShow: TvShow) {
-            // TODO: setup view's texts and click listeners with tvShow
+            Glide.with(mContext)
+                    .load(DisplayUtils.getImageUrl(tvShow.posterPath))
+                    .into(itemView.videoPoster)
+
+            itemView.videoTitle.text = tvShow.name
+            itemView.videoDescription.text = tvShow.summary
+            itemView.videoRating.text = tvShow.rating.toString()
+            itemView.productionYear.text = DisplayUtils.getYear(tvShow.firstAirDate)
+
+            itemView.likeButton.setOnClickListener { onLikeButtonClicked(itemView.likeButton, tvShow) }
+            itemView.setOnClickListener { onTvShowSelected(tvShow) }
+            itemView.videoPoster.setOnClickListener { onTvShowSelected(tvShow) }
+        }
+
+        private fun onLikeButtonClicked(view: ImageView, tvShow: TvShow) {
+
+            tvShow.isLiked = !tvShow.isLiked
+
+            if (tvShow.isLiked) {
+                view.setImageResource(R.drawable.ic_favorite)
+            } else {
+                view.setImageResource(R.drawable.ic_favorite_holo)
+            }
+        }
+
+        private fun onTvShowSelected(tvShow: TvShow) {
+            Toast.makeText(mContext, "${tvShow.name} Selected", Toast.LENGTH_SHORT)
+                    .show()
         }
     }
 }

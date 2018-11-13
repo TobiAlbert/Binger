ceac9507ef5ccded32192656162ec776dced3bc8
Tobi Daada <daadatobi@gmail.com>
2018-11-13 12:50:00 +0100

    Add API-Endpoints

--- app/src/main/java/ng/max/binger/utils/Constants.kt
+++ app/src/main/java/ng/max/binger/utils/Constants.kt
@@ -2,7 +2,12 @@ package ng.max.binger.utils
 
 object TMDB {
     // api key
-    private const val API_KEY_VALUE = "b3949470a8e2eb48fdded7ea54ea27df"
+    const val API_KEY_VALUE = "b3949470a8e2eb48fdded7ea54ea27df"
     const val  DEFAULT_PAGE_SIZE = 20 // default page_size for TMDB pagination
     const val API_KEY = "api_key=$API_KEY_VALUE"
+
+    // url endpoints
+    const val BASE_URL = "https://api.themoviedb.org/3/"
+    const val POPULAR_SHOWS = "${BASE_URL}tv/popular"
+    const val TODAY_SHOWS = "${BASE_URL}tv/airing_today"
 }

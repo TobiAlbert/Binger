dc50eb59c1cb06f7d3a6b9d06443505f5e38dd6f
Tobi Daada <daadatobi@gmail.com>
2018-11-14 11:44:16 +0100

    Add ApiEndPoint to Retrofit Service.

--- app/src/main/java/ng/max/binger/data/remote/TvShowService.kt
+++ app/src/main/java/ng/max/binger/data/remote/TvShowService.kt
@@ -3,8 +3,10 @@ package ng.max.binger.data.remote
 import io.reactivex.Observable
 import ng.max.binger.data.PagedList
 import ng.max.binger.data.TvShow
+import ng.max.binger.data.TvShowDetail
 import ng.max.binger.utils.TMDB
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface TvShowService {
 
@@ -13,4 +15,7 @@ interface TvShowService {
 
     @GET(TMDB.TODAY_SHOWS)
     fun getAiringTodayShows(): Observable<PagedList<TvShow>>
+
+    @GET(TMDB.TV_DETAILS)
+    fun getTvDetails(@Path("tv_id") tv_id: Int): Observable<TvShowDetail>
 }
